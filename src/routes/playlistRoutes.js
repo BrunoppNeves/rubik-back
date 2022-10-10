be35739acc5a1117918a25d1234c8235076dccbd
Luiza <luizagadelha@DTEC65048456.acad.unifor.br>
2022-10-10 13:52:33 -0300

    fix:get one playlist

--- src/routes/playlistRoutes.js
+++ src/routes/playlistRoutes.js
@@ -6,12 +6,12 @@ const authMiddleware = require('../middlewares/auth');
 
 router.get("/get_one_public/:id", playlistController.getOnePublic);
 router.get("/get_all_public", playlistController.getAllPublic);
+router.get("/get_playlist/:id", playlistController.getOnePlaylist);
 
 router.use(authMiddleware);
 router.post("/create", multer.single("image"), playlistController.create);
 router.get("/get_all_private/:user_id", playlistController.getAllPrivate);
 router.get("/get_one_private/:user_id/:playlist_id", playlistController.getOnePrivate);
-router.get("/get_playlist/:id", playlistController.getOnePlaylist);
 
 router.put("/update/:id", playlistController.updatePlaylist);
 router.put("/addSong", playlistController.addSong);
